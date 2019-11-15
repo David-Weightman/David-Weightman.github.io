78a6946288869e6a7312d878ecdcece043c2e08f
David-Weightman <56867844+David-Weightman@users.noreply.github.com>
2019-11-15 12:33:13 +0000

    updated news script
    this news script splits the headlines and calculates the most common word

--- softeng.py
+++ softeng.py
@@ -6,4 +6,21 @@ url = ('https://newsapi.org/v2/top-headlines?'
        'country=us&'
        'apiKey=a8a1a5ea66c04f1488210e7b0016b948')
 response = requests.get(url)
-print (response.json())
+#print (response.json())
+
+words = ''
+
+for i in response.json()['articles']:
+    words += i['title'] + ' '
+
+    #print(i['title'])
+
+print(words)
+
+words2 = words.split()
+
+Counter = Counter(words2)
+
+most_occur = Counter.most_common(4)
+
+print(most_occur)
